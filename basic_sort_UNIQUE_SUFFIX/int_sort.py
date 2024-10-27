ae3d98e0d1c17c7a147886212de84e76e6688eb8
Sophie Walden <seamus.walden@maine.edu>
2024-10-27 15:33:43 -0400

    Created insertion sort

--- basic_sort_UNIQUE_SUFFIX/int_sort.py
+++ basic_sort_UNIQUE_SUFFIX/int_sort.py
@@ -17,14 +17,19 @@
 # =========================================================================
 
 """
-This module sorts lists of integers...
+This module contains three separate functions for sorting lists of numbers: 
+ - Bubble Sort
+ - Quick Sort
+ - Insertion Sort
 """
 
 
 def bubble(int_list: list) -> list:
     """
     Runs bubble sort on inputted list, by doing the following
-    1: 
+    1: Iterates through the input list element by element
+    2: If a value is higher then its adjacent it swaps the two
+    3: This process is repeated until every element has had enough swaps to be properly sorted
 
     Params:
         int_list <List>: input list
@@ -39,6 +44,7 @@ def bubble(int_list: list) -> list:
             # Swap value if it is higher then adjacent value
             if int_list[i] > int_list[i + 1]:
                 int_list[i], int_list[i + 1] = int_list[i + 1], int_list[i]
+
     return int_list
 
 
@@ -84,6 +90,28 @@ def quick(int_list):
 
 def insertion(int_list):
     """
-    insertion docstring
+    Runs insertion sort on inputted list, by doing the following
+    1: Iterates through the input list element by element
+    2: If a value is lower then its adjacent it swaps the two
+    3: This process is repeated until every element has had enough swaps to be properly sorted
+
+    The way that this differentiates from bubble sort is the order it swaps values in.
+    By starting at every element and going backwards it created a sorted subarray at the begging.
+
+    For instance, when the outer loop is at index 3, we know indexes 0-2 are a properly sorted subarray
+
+    Params:
+        int_list <List>: input list
+
+    Returns:
+        <List>: Sorted version of input list
     """
-    print("insertion sort")
+
+    for j in range(len(int_list)):
+        for i in range(j, 0, -1):
+
+            # Swap value if it is lower then adjacent value
+            if int_list[i] < int_list[i - 1]:
+                int_list[i], int_list[i - 1] = int_list[i - 1], int_list[i]
+
+    return int_list
